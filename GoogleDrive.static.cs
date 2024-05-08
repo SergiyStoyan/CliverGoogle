9960d69c32c92df8e429b22ebaca9d0c9f0e26f7
Sergiy Stoyan <s.y.stoyan@gmail.com>
2024-05-08 23:05:31 +0300

    5.2.1
    - ExtractObjectIdFromWebLink() improved;

--- GoogleDrive.static.cs
+++ GoogleDrive.static.cs
@@ -28,7 +28,7 @@ namespace Cliver
         public static string ExtractObjectIdFromWebLink(string webLink)
         {
             //webLink = Regex.Replace(webLink.Trim(), @"/(edit|view)[^/]*\s*$", "", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-            Match m = Regex.Match(webLink, @"/([^/]+?)((\?|/(edit|view))[^/]*)\s*$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+            Match m = Regex.Match(webLink, @"drive\.google\.com.*?/([^/]+?)((\?|/(edit|view))[^/]*)?\s*$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
             if (!m.Success)
                 throw new Exception("Could not parse the link: " + webLink);
             return m.Groups[1].Value;
